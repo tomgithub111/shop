5fe467c30d0685489c587687b32f422f175934de
tomgithub111 <1924562769@qq.com>
2022-09-09 09:21:10 +0800

    add files

---
+++ js.js
@@ -0,0 +1,16 @@
+	function okClick(name) {
+		
+		var s=document.getElementsByName(name);
+
+		alert(s[0].value);
+	}
+
+	function noclick(id){
+
+       alert(document.getElementById(id).value);
+	}
+
+
+	function show(){
+		alert("测试");
+	}
